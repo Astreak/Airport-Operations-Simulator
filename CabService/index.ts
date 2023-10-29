62fa0ab6650bbd07175d898ed221f3ac89c4101d
A@G <dpovertree@gmail.com>
2023-10-29 23:53:18 +0530

    Integrated database connection, tested connection

--- CabService/index.ts
+++ CabService/index.ts
@@ -1,3 +1,28 @@
 import express from "express";
 import db from './src/config/db';
-const app = express();
+import app  from "./src/router/app";
+var port = process.env.PORT || '5000';
+app.get('/', (req, res) => {
+    res.send('Hello, this is your Node.js server connected to MongoDB!');
+});
+app.get('/post',(req,res)=>{
+    let obj = new db({
+        "empId":'asdasdasd',
+        "firstName":'John',
+        "lastName": 'OP',
+        "cabId": 'opasdasd',
+        "isOccupied": 'NO',
+        "onRide": 'YES'
+    });
+    obj.save()
+        .then((doc)=>{
+            console.log("Document Saved ",doc);
+        }).catch((err)=>{
+            console.log("Error ",err);
+        })
+})
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});

2df937122283369e1d832a015dc340abb04ecbb4
A@G <dpovertree@gmail.com>
2023-11-06 16:57:38 +0530

    Changed Layout

--- CabService/app.ts
+++ CabService/app.ts
@@ -1,8 +1,19 @@
-import express from "express";
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
 import db from './src/config/db';
-import app from "./src/router/app";
 import { checkDriverStatus } from "./src/middlewares/mid1";
-var port = process.env.PORT || '5000'; 
+import 'dotenv/config'
+const dbUri = process.env.DB_URI || 'None';
+const port = process.env.PORT || '5000'; 
+const connectDB: Promise<void> = mongoose.connect(dbUri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+} as any).then(()=>{
+        console.log("Database Connected");
+}).catch((err)=>{
+    console.log(err);
+});
+var app = express();
 app.get('/', (req, res) => { // executes one middleware after another and both have access to the reponse and request object;
     res.send('Hello, this is your Node.js server connected to MongoDB!');
 });
@@ -24,7 +35,10 @@ app.put('/bookCab', checkDriverStatus, (req, res) => {
             res.send('Error');
         });
 });
-
+//OTP logic have to be implemented
+app.put('/endRide', (req, res) => {
+    
+});
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);

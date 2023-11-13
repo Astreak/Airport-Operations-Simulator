6b39af3f590ea763121c7562663dd37acc76d7bb
A@G <dpovertree@gmail.com>
2023-11-13 22:01:39 +0530

    Fixed the User cancellation

--- CabService/src/controllers/cancelRideController.ts
+++ CabService/src/controllers/cancelRideController.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import { cabdb, ridedb } from '../config/db';
 import { setDriverInactive, getDriverIdUserCancellation} from '../middlewares/deleteRideInfo';
 const userId = '6asdbasdxsadasXjasdnasd';
@@ -11,17 +11,18 @@ var cancelRideCab = async (req: any, res: any) => {
     // get the current driver ID and remove the hardcoded one
     let responseMessage:any = await setDriverInactive(driverId);
     if (responseMessage == '0')
-        res.send(201, 'Your Ride has been cancelled');
+        res.status(201).send('Your Ride has been cancelled');
     else
-        res.send(504, 'Server Error :(');
+        res.status(504).send('Server Error :(');
 }
 var cancelRideUser = async (req: any, res: any) => {
     let getDriverID: cVoid = await getDriverIdUserCancellation(userId);
-    let responseMessage:any = await setDriverInactive(driverId);
+    let responseMessage: string | void = await setDriverInactive(getDriverID);
+    console.log(responseMessage);
     if (responseMessage == '0')
-        res.send(201, 'Your Ride has been cancelled');
+        res.status(201).send('Your Ride has been cancelled');
     else
-        res.send(504, 'Server Error :(');
+        res.status(504).send('Server Error :(');
     
 }
 export { cancelRideCab, cancelRideUser };
--- CabService/src/middlewares/deleteRideInfo.ts
+++ CabService/src/middlewares/deleteRideInfo.ts
@@ -6,33 +6,34 @@ const to = "Park Street";
 const tripCost = 400;
 const driverId = 'XYZ1';
 // Make the current Driver activity setup to inactive
-enum SuccessInfo{ SUCCESS = '0', FAILURE = '1'}
-let setDriverInactive = (driverId:string): Promise<SuccessInfo | void> => {
-      return cabdb.findOne({id: driverId})
+enum SuccessInfo { SUCCESS = '0', FAILURE = '1' };
+let setDriverInactive = (driverId: string | void): Promise<string | void> | void => {
+     cabdb.findOne({id: driverId})
         .then((d) => {
             if (d != null) {
                 d.onRide = false;
                 d.save();
-                ridedb.deleteOne({ driverId: driverId, pickup: false, active: true }) // returns deleteResult object
-                    .then((d) => {
-                        console.log('Ride record deleted');
-                        return SuccessInfo.SUCCESS;
-                    })
-                    .catch((e) => {
-                        console.log(e);
-                        return SuccessInfo.FAILURE;
-                    });
+                
                 //res.send("The ride is cancelled");
             } else {
-                throw new Error("Something went wrong");
+                throw new Error("[-]Something went wrong");
                 //return SuccessInfo.FAILURE;
             }
         })
         .catch((e) => {
             console.log(e);
-            return SuccessInfo.FAILURE;
+            return ;
             //next();
-        });    
+        }); 
+    return ridedb.deleteOne({ driverId: driverId, pickup: false, active: true }) // returns deleteResult object
+            .then((d) => {
+                console.log('[+] Ride record deleted');
+                return '0';
+            })
+            .catch((e) => {
+                console.log(e);
+                return '1';
+            });
 }
 let getDriverIdUserCancellation = (userId:string): Promise<string | undefined > => {
          return ridedb.findOne({ customerId: userId, pickup: false, active: true })

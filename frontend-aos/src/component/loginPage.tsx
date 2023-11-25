f531e4c8bbba9cf62c9093ccb41f5a8224de6587
A@G <dpovertree@gmail.com>
2023-11-25 13:06:25 +0530

    Added ATC config

---
+++ frontend-aos/src/component/loginPage.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/loginPage.css';
+class LoginPage extends Component{
+    render() {
+        return (
+            <div>
+                <h1> OK BOOMER</h1>
+            </div>
+        )
+    }
+}
+export { LoginPage };
